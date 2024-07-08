10da87ad7a04f4b3e81e5604ae3c4fbb869a67ba
EternalRival <eternalrival8@gmail.com>
2024-07-08 23:38:30 +0300

    feat: add more projects

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -21,9 +21,28 @@ const projectsProps = [
     isTeamProject: true,
   },
   {
+    label: 'what do you meme',
+    sourceHref: 'https://github.com/EternalRival/RSSchool-JSFE2022Q3/tree/main/what-do-you-meme/server',
+    isTeamProject: true,
+  },
+  {
     label: 'virtual-keyboard',
     sourceHref: 'https://github.com/EternalRival/virtual-keyboard',
     deployHref: 'https://eternalrival.github.io/virtual-keyboard/',
+  },
+  {
+    label: 'rsschool-tools',
+    sourceHref: 'https://github.com/EternalRival/er-rsschool-tools',
+    deployHref: 'https://er-rsschool-tools.vercel.app/',
+  },
+  {
+    label: 'rsschool-gratitude-summary',
+    sourceHref: 'https://github.com/EternalRival/rsschool-gratitude-summary',
+    deployHref: 'https://private-user-images.githubusercontent.com/59611223/263753501-ff506148-8db5-4f5f-acd9-7c86923feb9f.gif?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MjA0NTYzMTUsIm5iZiI6MTcyMDQ1NjAxNSwicGF0aCI6Ii81OTYxMTIyMy8yNjM3NTM1MDEtZmY1MDYxNDgtOGRiNS00ZjVmLWFjZDktN2M4NjkyM2ZlYjlmLmdpZj9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDA3MDglMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwNzA4VDE2MjY1NVomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWU5NWQwMDQ1YzQ5OTlkYWVhMTc3Y2UxMTlhZDgxYzE2MjAxMmVlMjJiMzQ4YmZiNDQ3Y2YzNDc0ODg4MTU3NDAmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.HGVF6EvOeeoA8--yXx3PfopUMzNvkW9zZh6F5FJEoI4',
+  },
+  {
+    label: 'home-library-service',
+    sourceHref: 'https://github.com/EternalRival/RSSchool-NodeJS2023Q2/tree/main/07-home-library-service',
   },
   {
     label: 'graphiql-app',
